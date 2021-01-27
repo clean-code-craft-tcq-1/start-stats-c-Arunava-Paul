686d0346b3cb5f9ea30b03b009bca70be756747f
Arunava-Paul <77390499+Arunava-Paul@users.noreply.github.com>
2021-01-27 14:43:22 +0530

    Update alerter.h

--- alerter.h
+++ alerter.h
@@ -9,5 +9,5 @@
 #define NO_OF_ALERTS 2
 typedef void (*alerter_funcptr[2])(void);
 void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats);
-extern void emailAlerter(void);
-extern void ledAlerter(void);
+void emailAlerter(void);
+void ledAlerter(void);
