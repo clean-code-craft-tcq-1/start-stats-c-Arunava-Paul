c15309053f446cdf7e398f47baa62e815f2459e0
Arunava-Paul <77390499+Arunava-Paul@users.noreply.github.com>
2021-01-27 14:36:21 +0530

    Update alerter.h

--- alerter.h
+++ alerter.h
@@ -4,8 +4,9 @@
  *  Created on: Jan 24, 2021
  *      Author: PUL1KOR
  */
-#define NULL 0
-#define NULL_PTR  (void*)0
+
+//#define NULL_PTR  (void*)0
+#define NO_OF_ALERTS 2
 typedef void (*alerter_funcptr[2])(void);
 void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats);
 extern void emailAlerter(void);
