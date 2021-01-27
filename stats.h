126c1e57c287e7a6420b1bb11cf12ab1c5e86364
Arunava-Paul <77390499+Arunava-Paul@users.noreply.github.com>
2021-01-27 14:23:13 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -10,8 +10,5 @@ struct Stats
 
 struct Stats compute_statistics(const float* numberset, int setlength);
 
-typedef void (*alerter_funcptr[2])(void);
-
-
 extern int emailAlertCallCount;
 extern int ledAlertCallCount;
